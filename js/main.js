f196fbd079363d208748e72923b8a07c46b56b3c
olimp-50 <gugl6369@gmail.com>
2026-02-03 14:30:41 +0700

    add(index.html, main.js): Computed properties 8, practic 7

--- js/main.js
+++ js/main.js
@@ -4,29 +4,30 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: 'A pair of warm, fuzzy socks',
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         // link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",  ЗАДАНИЕ 2
         inStock: true,
-        // onSale: true, ЗАДАНИЕ 3
+        // onSale: true,    ЗАДАНИЕ 3, 5
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
         ],
         cart: 0,
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
         // sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'], ЗАДАНИЕ 4
 
     },
@@ -34,8 +35,23 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        removeFromCart(){
-            if (this.cart > 0) this.cart -= 1;
+        // removeFromCart(){
+        //     if (this.cart > 0) this.cart -= 1; ЗАДАНИЕ 5
+        // },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        // sale() { ЗАДАНИЕ 7
+        //     return (this.onSale ? 'skidka net ' : 'skidka est ') + ' this ' + this.product + ' for brand: ' + this.brand;
+        // },
+    }
 })
