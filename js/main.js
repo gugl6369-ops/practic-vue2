d1e7ab9b7f0827770ff1d92c309fc74fbf2007a3
elfi <gugl6369@gmail.com>
2026-02-04 21:18:59 +0000

    add: start cart

--- js/main.js
+++ js/main.js
@@ -2,32 +2,62 @@ let eventBus = new Vue()
 
 Vue.component('cart', {
     props: {
-        data: [
-
-        ],
+        cart:{
+            type: Array,
+            required: true
+        }
     },
     template: `
-                <article>
+                <article class="board">
                     <div>
-                        <img :src="icon">
+                        <img :src="assets/icon.png"> 
+                        <p> {{name}} </p>
+                    </div> 
+                    <div>
+                        <div class="task">
+                            <img src="assets/icon.png">
+                            <p>{{4}} of {{5}}</p> 
+                            <label>
+                                <progress value="progress" max="100"></progress>
+                            </label>
+                            <p> {{progress}}% </p>
+                        </div>
+                        <div>
+                            <input type="checkbox"></input>
+                        </div> 
                     </div>
                     <div>
-                        <p>{{name}}</p>    
-                    </div>
-                    <div>
-                        <label>
-                            Прогресс:
-                            <progress value="progress" max="100"></progress>
-                        </label>
+                        
                     </div>  
                 </article>
-              `
+              `,
+    data() {
+        return {
+            progress: 10,
+            name: 'name',
+
+        }
+    }
+})
+
+Vue.component('board', {
+    template: `
+                <div class="board">
+                    
+                </div>
+              `,
+    data() {}
 })
 
 let app = new Vue({
     el: '#app',
     data: {
         cart: [],
+        boards: {
+            todo: [],
+            progress: [],
+            done: [],
+        },
     }
 })
 
