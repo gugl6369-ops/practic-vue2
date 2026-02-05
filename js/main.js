571c3703c2569820fccc06886e4cb3bce4152ceb
elfi <gugl6369@gmail.com>
2026-02-05 20:25:53 +0000

    add(style.css, main.js, index.html): add filter for cart

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 let eventBus = new Vue()
 
+Vue.component('form', {
+    template: `
+                 <div>
+                    <form class="form">
+                        <label for="name">Name
+                            <input type="text" class="form__input">
+                        </label>
+                        <div>
+                            <ladel @click="form__label">
+                                <input type="text" v-model="name" class="form__input">
+                            </ladel>    
+                        </div>
+                        <button type="button">Добавить чекпоинт</button>
+                        <input>
+                    </form>
+                 </div>
+              `,
+    data(){
+        return{
+            form:{
+
+            }
+        }
+    }
+})
+
 Vue.component('cart', {
     props: {
         cart:{
@@ -35,29 +61,74 @@ Vue.component('cart', {
         return {
             progress: 10,
             name: 'name',
-
         }
     }
 })
 
 Vue.component('board', {
+    props: {
+        cart:{
+            type: Array
+        },
+        index:{
+            type: Number,
+        }
+    },
     template: `
                 <div class="board">
-                    
+                     <template v-if="cart.length">
+                     <p>{{index}}</p>
+                        <div>
+                            <template v-for="item in getCart()">
+                                <p>{{item}}</p>
+                            </template>
+                        </div>
+                     </template>
+                     <template v-else>
+                        <p>нет ничего</p>
+                     </template>
                 </div>
               `,
-    data() {}
+    data() {
+
+    },
+    methods: {
+        getCart(){
+            return this.cart.filter(item=> item.status == this.index );
+        }
+    }
 })
 
 let app = new Vue({
     el: '#app',
     data: {
-        cart: [],
+        cart: [
+            {
+                name: 'имя',
+                point: 'пункт',
+                status: 1,
+            },
+            {
+                name: 'имя2',
+                point: 'пункт',
+                status: 2,
+            },
+            {
+                name: 'имя3',
+                point: 'пункт',
+                status: 3,
+            },
+        ],
         boards: {
-            todo: [],
-            progress: [],
-            done: [],
+            1: [],
+            2: [],
+            3: [],
         },
+    },
+    methods: {
+        addCart(){
+
+        }
     }
 })
 
